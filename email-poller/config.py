c6f0ee9b62d95187c6eabd4ba79ed186344729e9
jhull123 <tarballer01@gmail.com>
2025-04-19 10:14:03 -0500

    get email poller lambda app up and running on aws

--- email-poller/config.py
+++ email-poller/config.py
@@ -1,8 +1,25 @@
+import os, pickle
+from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+TOKEN_PATH = 'token.pickle'
 
 def get_credentials():
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-    return flow.run_local_server(port=0)
+    creds = None
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token_file:
+            creds = pickle.load(token_file)
 
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open(TOKEN_PATH, 'wb') as token_file:
+            pickle.dump(creds, token_file)
+
+    return creds

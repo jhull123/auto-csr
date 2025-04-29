9b24fd3e0a391f8706020eaab2532b28bbab9c68
jhull123 <tarballer01@gmail.com>
2025-04-28 20:04:23 -0500

    incorporate email writer into agent

--- csr_agent/agent.py
+++ csr_agent/agent.py
@@ -1,8 +1,11 @@
 import json
 import boto3
 from email_categorizer.email_categorizer import EmailCategorizer
+from email_writer.email_writer import EmailWriter
 
 s3 = boto3.client('s3')
+email_writer = EmailWriter()
+
 
 def new_email(event, context):
     for record in event['Records']:
@@ -34,3 +37,17 @@ def new_email(event, context):
 def categorize_email(email_body: str) -> str:
     email_categorizer = EmailCategorizer()
     return email_categorizer.categorize_email(email_body)
+
+def process_email(email_category: str, email_body: str) -> None:
+    match email_category:
+        case "help with return":
+            email_response = email_writer.write_customer_return_email()
+            print("Email response:")
+            print(email_response)
+            return
+        case "customer complaint":
+            return
+        case "request for information":
+            return
+        case "spam":
+            return
